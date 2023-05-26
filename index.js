ba2660ce51ce22ece0105340fd595e55811aa35c
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-26 09:18:47 +0800

    DOM

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const num = 1;
+const landingPage = document.querySelector(".landing-page");
 
-console.log(num);
+const myToDoList = document.querySelector("#my-to-do-list");
+
+const nameInput = document.querySelector("#name-input");
+
+const submit = document.querySelector("#submit");
+
+const mainPage = document.querySelector(".main-page");
+
+const userName = document.querySelector("#user-name");
+
+const addItem = document.querySelector("#add-item");
+
+const removeItem = document.querySelector("#remove-item");
+
+const exit = document.querySelector("#exit");
