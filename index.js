b0ee01b59445cd6f96dff49c83300aab2a4645da
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-28 00:23:10 +0800

    ADDED render FUNCTION TO removeItem BUTTON

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const nameInputAndBGChanger = function () {
 submit.addEventListener("submit", nameInputAndBGChanger());
 
 // CREATED A FUNCTION THAT ADDS THE TASK TYPED IN THE TO DO LIST INPUT
-
 const addTask = function () {
   // Created a variable that store the name typed in the input element
   const item = addItem.value.trim();
@@ -42,15 +41,25 @@ const addTask = function () {
 // ADDED addTask FUNCTION TO ADDITEM BUTTON
 addItem.addEventListener("submit", addTask());
 
-// CREATED A removeItem FUNCTION TO REMOVE ITEM BUTTON
-
-const erase = function () {
-  // Created a variable that splices or removes the clicked to do list item.
-  const del = removeItem.splice(i, 1);
-  // Invoke the erase function to update the list visually
-  erase();
-};
-
-removeItem.addEventListener("click", erase());
-
 // CREATED A FUNCTION THAT RENDERS THE list
+const render = function () {
+  // This line clears the existing content of the list turning it to an empty string
+  list.innerHTML = "";
+  // This line is for the repition over each item in the toDoList
+  for (let i = 0; i < toDoList.length; i++) {
+    const item = toDoList[i];
+    // create new list element
+    const newLi = document.createElement("newLi");
+    // insert the text typed to the item variable or to do list.
+    newLi.innerText = item;
+    // Created a variable that splices or removes the clicked to do list item.
+    const del = removeItem.splice(i, 1);
+    render();
+  }
+};
+// ADD THE removeItme to the newLi element
+newLi.appendChild(removeItem);
+// ADD THE newLi to the list element
+list.appendChild(newLi);
+// ADDED render FUNCTION TO removeItem BUTTON
+removeItem.addEventListener("click", render());
