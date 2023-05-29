27c1e1c92516a6800f2d47d4b83c2f7f8577fa05
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-29 14:09:46 +0800

    added JS

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+// LANDING PAGE SECTION
+const landingPage = document.querySelector("#landingPage")
+const userName = document.querySelector("#userName")
+
+
+//MAIN PAGE SECTION
+const mainPage = document.querySelector("#mainPage")
