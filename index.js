6a81dd3edf2f22fb76e3461026c61fb4e0fd9d57
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-29 20:25:34 +0800

    added JS

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-'use strict'
+"use strict";
+
+const todo = document.querySelector(".todo");
+const form = document.querySelector("form");
+const userInput = document.querySelector("#userInput");
+const submitBtn = document.querySelector("#submitBtn");
+const list = document.querySelector("#list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = userInput.value;
+  const newLi = document.createElement("li");
+  newLi.innerText = input;
+  list.append(newLi);
+  form.reset();
+});
