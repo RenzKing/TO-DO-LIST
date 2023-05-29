7db5d0edcc89f3e34fcb98d5b2f6d9afa07da76a
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-29 14:42:17 +0800

    redo to do list again

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 'use strict'
-// LANDING PAGE SECTION
-const landingPage = document.querySelector("#landingPage")
-const userName = document.querySelector("#userName")
-
-
-//MAIN PAGE SECTION
-const mainPage = document.querySelector("#mainPage")
