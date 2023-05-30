51f0c80e110b40f8ac8e3bd2f5df4630eef4599e
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-31 01:07:27 +0800

    to do list

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-// ASSIGNING ELEMENTS
-const landingPage = document.querySelector(".landing-page");
+`user strict`;
 
-const myToDoList = document.querySelector("#my-to-do-list");
+const userInput = document.querySelector("#userInput");
+const submitBtn = document.querySelector("#submitBtn");
+const list = document.querySelector("#list");
 
-const nameInput = document.querySelector("#name-input");
+const todo = () => {
+  //1. get the text from the input field
+  const input = userInput.value.trim();
+  //2. create a new "li" element
+  const newLi = document.createElement("li");
+  //3. create a text node to print the text typed in the input field
+  const text = document.createTextNode(input);
 
-const submit = document.querySelector("#submit");
-
-const mainPage = document.querySelector(".main-page");
-
-const userName = document.querySelector("#user-name");
-
-const addItem = document.querySelector("#add-item");
-
-const removeItem = document.querySelector("#remove-item");
-
-const exit = document.querySelector("#exit");
-
-let toDoList = [];
-
-submit.addEventListener("submit", function () {
-    // Create a variable that store the name typed in the input element
-    const name = nameInput.value.trim();
-
-    // This checks if the name is true or it contains a non-empty value so that if it is true it will run the following block of code
-        if(userName) {
-    // this line gets the value or typed in the name variable
-    userInput.innterText = name;        
-    }
-})
+  //4. Make sure that something is typed in the input field. If there is none, then nothing will happen.
+  if (input === "") {
+    return;
+  }
+  //5. Added the text variable inside the newLi variable
+  newLi.appendChild(text);
+  //6. Added the newLi variable inside the List variable
+  list.appendChild(newLi);
+  // 7. Clear the input field
+  userInput.value = "";
+};
+// 8. Added event listener for the submitBtn button
+submitBtn.addEventListener("click", todo);
